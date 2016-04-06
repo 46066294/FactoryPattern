14b7cf6929744a64d71048a1a70d232acb846815
46066294 <46066294p@iespoblenou.org>
2016-04-06 23:43:41 +0200

    Products and Creators

---
+++ PatronFactory/ConcreteCreatorA.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatronFactory
+{
+    class ConcreteCreatorA : Creator
+    {
+        public Product FactoryMethod()
+        {
+            return new ConcreteProductA();
+        }
+    }
+}
